93271e39501d04be2cc5964701f41bd5593b5ea8
Patima Poochai <patimapoochai@gmail.com>
2024-09-02 16:06:08 -1000

    Add frontend web and IaC files

---
+++ cloud-resume-frontend/main.tf
@@ -0,0 +1,134 @@
+locals {
+  s3_origin_id = "cloud_resume_project_origin"
+}
+
+module "frontend_directory" {
+  source   = "hashicorp/dir/template"
+  version  = "1.0.2"
+  base_dir = "${path.module}/${var.website_files_path}"
+}
+
+
+resource "aws_s3_bucket" "cloud_resume_bucket" {
+  bucket = var.bucket-name
+}
+
+resource "aws_s3_object" "frontend_files" {
+  # from https://stackoverflow.com/questions/57456167/uploading-multiple-files-in-aws-s3-from-terraform
+  for_each     = module.frontend_directory.files
+  bucket       = aws_s3_bucket.cloud_resume_bucket.bucket
+  key          = each.key
+  source       = each.value.source_path
+  content_type = each.value.content_type
+}
+
+resource "aws_s3_bucket_policy" "cloudfront_access_only" {
+  bucket = aws_s3_bucket.cloud_resume_bucket.id
+  policy = data.aws_iam_policy_document.cloud_resume_bucket_policy.json
+}
+
+data "aws_iam_policy_document" "cloud_resume_bucket_policy" {
+  version   = "2008-10-17"
+  policy_id = "PolicyForCloudFrontPrivateContent"
+
+  statement {
+    sid    = "AllowCloudFrontServicePrincipal"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = ["s3:GetObject"]
+
+    resources = ["${aws_s3_bucket.cloud_resume_bucket.arn}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = [aws_cloudfront_distribution.cloud_resume_frontend_distribution.arn]
+    }
+
+  }
+}
+
+resource "aws_cloudfront_distribution" "cloud_resume_frontend_distribution" {
+  enabled = true
+
+  origin {
+    domain_name              = aws_s3_bucket.cloud_resume_bucket.bucket_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.cloudfront_oac.id
+    origin_id                = local.s3_origin_id
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    viewer_protocol_policy = "redirect-to-https"
+    target_origin_id       = local.s3_origin_id
+    cache_policy_id        = data.aws_cloudfront_cache_policy.Managed_CachingOptimized.id
+    compress               = true
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = data.aws_acm_certificate.patimapoochai_net_certificate.arn
+    ssl_support_method       = "sni-only" # set this to static-ip for a fun interview story
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  aliases             = var.website_aliases
+  default_root_object = "index.html"
+
+}
+
+data "aws_acm_certificate" "patimapoochai_net_certificate" {
+  domain   = "patimapoochai.net"
+  statuses = ["ISSUED"]
+}
+
+data "aws_cloudfront_cache_policy" "Managed_CachingOptimized" {
+  name = "Managed-CachingOptimized"
+}
+
+resource "aws_cloudfront_origin_access_control" "cloudfront_oac" {
+  name                              = aws_s3_bucket.cloud_resume_bucket.bucket_domain_name
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+data "aws_route53_zone" "patimapoochai_net_zone" {
+  name = "patimapoochai.net."
+}
+
+resource "aws_route53_record" "non_aliased_records" {
+  for_each = toset(var.website_aliases)
+
+  zone_id = data.aws_route53_zone.patimapoochai_net_zone.zone_id
+  name    = each.key
+  type    = "CNAME"
+  ttl     = 300
+
+  records = [aws_cloudfront_distribution.cloud_resume_frontend_distribution.domain_name]
+}
+
+resource "aws_route53_record" "records" {
+  zone_id = data.aws_route53_zone.patimapoochai_net_zone.zone_id
+  name    = var.domain_name
+  type    = "A"
+
+  alias {
+    zone_id                = aws_cloudfront_distribution.cloud_resume_frontend_distribution.hosted_zone_id
+    name                   = aws_cloudfront_distribution.cloud_resume_frontend_distribution.domain_name
+    evaluate_target_health = false
+  }
+
+}
---
+++ cloud-resume-frontend/src/app/page.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import styles from "./page.module.css";
+
+const SectionItem = ({ title, date, children }) => {
+  return (
+    <div className={styles.sectionItem}
+      style={date && { display: 'grid', gridTemplateColumns: 'repeat(2,1fr)' }}>
+      <h2>{title}</h2>
+      {date && <p style={{ fontSize: '1.5rem', fontWeight: 'bold', textAlign: 'right' }}>{date}</p>}
+      {children}
+    </div>
+  );
+}
+
+const Section = ({ title, children }) => (
+  <section className={styles.section}>
+    <h1 className={styles.sectionHeader}>{title}</h1>
+    {children}
+  </section>
+);
+
+export default function Home() {
+  return (
+    <main className={styles.container}>
+      <header className={styles.header}>
+        <h1>Patima Poochai</h1>
+        <p className={styles.headerSubtitle}>Lihue, HI 96766
+          | patimap (at) protonmail.com
+          | <a href="http://www.linkedin.com/in/patima-poochai808">Linkedin</a></p>
+      </header>
+      <Section title="Accomplishments">
+        <SectionItem title="Linux administration">
+          <ul className={styles.ul}>
+            <li>Installed and configured 3 clustered Linux virtualization servers and 1 pfSense router.
+              Created virtual machine images and provisioned virtual machines.</li>
+            <li>Competed in a 48-hour IT administration and incident response competition as an administrator
+              overseeing 5 Linux servers. Maintained IT policy compliance by identifying and removing unauthorized programs and patching Linux services and user applications.</li>
+          </ul>
+        </SectionItem>
+        <SectionItem title="Network administration">
+          <ul className={styles.ul}>
+            <li>Set up the networking infrastructure for an IT administration lab serving 15 students by
+              configuring OpenVPN for remote access, managing remote access credentials, and
+              troubleshooting user access issues.</li>
+          </ul>
+        </SectionItem>
+        <SectionItem title="Teamwork and collaboration">
+          <ul className={styles.ul}>
+            <li>Coordinated the daily operations of a student club educating over 100 registered members on
+              cybersecurity topics by planning club logistics and coordinating with 5 board members to
+              facilitate events.</li>
+          </ul>
+        </SectionItem>
+        <SectionItem title="Desktop Support">
+          <ul className={styles.ul}>
+            <li>Deployed over 300 windows computers using Active directory and MECM. Exceeded the target
+              goal by 40 computers.</li>
+          </ul>
+        </SectionItem>
+      </Section>
+      <Section title="Work History">
+        <SectionItem title="TEKsystems" date="Aug 2023 - Present">
+          <p>Desktop Technician</p>
+        </SectionItem>
+        <SectionItem title="US Air Force & University of Hawaii at Manoa" date="Aug 2022 - Dec 2022">
+          <p>Technical Lead</p>
+        </SectionItem>
+      </Section>
+      <Section title="Certifications">
+        <ul className={styles.ul}>
+          <li><a href="https://rhtapps.redhat.com/verify?certId=240-145-797">Red Hat Certified System Administrator</a></li>
+          <li><a href="https://www.credly.com/badges/674b29e8-c49a-41bb-99c7-e35518cf4c50/public_url">CompTIA Security+</a></li>
+          <li><a href="https://www.credly.com/badges/bae02fcc-35bf-4911-a878-56dcefc812cd/public_url">CompTIA Network+</a></li>
+          <li><a href="https://cp.certmetrics.com/amazon/en/public/verify/credential/d578d3caf2374818b395da7d4cc5b759">AWS Certified Cloud Practitioner</a></li>
+        </ul>
+      </Section>
+      <Section title="Education">
+        <SectionItem title="B.A. Information and Computer Science" date="2018 - 2022">
+          <p>University of Hawaii at Manoa, HI</p>
+        </SectionItem>
+      </Section>
+    </main>
+  );
+}
