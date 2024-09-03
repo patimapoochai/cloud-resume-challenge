76684c219e3bbba8f3f5f9403474ee75cf1b6966
Patima Poochai <patimapoochai@gmail.com>
2024-09-02 17:11:31 -1000

    Update cloud-resume-frontend terraform file

--- variables.tf
+++ variables.tf
@@ -1,4 +1,22 @@
-variable "aws_profile" {
-  type = string
-  description = "Profile name obtained via AWS cli sso command"
+variable "bucket-name" {
+  type        = string
+  description = "The name of the s3 bucket for static website"
+  default     = "patima-cloud-resume"
+}
+
+variable "website_aliases" {
+  type    = list(string)
+  default = ["resume.patimapoochai.net"]
+}
+
+variable "website_files_path" {
+  type        = string
+  description = "Relative path to the production build of the static site, no front or ending forward slashes"
+  default     = "out"
+}
+
+variable "domain_name" {
+  type        = string
+  description = "The fully qualified domain name of the website"
+  default     = "patimapoochai.net"
 }
