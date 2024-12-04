bb6a4e92d32660a5a328e9fdfab008844a928b9a
Patima Poochai <patimapoochai@gmail.com>
2024-12-03 18:29:56 -1000

    Add tf infrastructure folder

---
+++ modules/infrastructure/README.md
@@ -0,0 +1,1 @@
+This directory is for terraform modules used to set up the deployment to a fresh AWS account
---
+++ modules/infrastructure/main.tf
@@ -0,0 +1,7 @@
+# open id connect provider
+## if there is already a provider named X, don't deploy this provider
+## instead reference the provider as data
+
+# remote state for infrastructure
+
+# github actions role
